561da1daabbc52e983ec40ef0186fcccc73f9ec4
Caleb Manor <calebmanorprogramming@gmail.com>
2024-06-11 16:29:32 +0000

    Added a basic argument parser for the script

--- convertPipelineToEcho.sh
+++ convertPipelineToEcho.sh
@@ -7,6 +7,37 @@ main() {
 	secondInput=""
 	term=""
 
+	while getops ":i:o:de:t:h" opt; do
+		case ${opt} in
+			i)
+				inputPath="${OPTARG}"
+				;;
+			o)
+				outputPath="${OPTARG}"
+				;;
+			d)
+				getDept=true
+				;;
+			e)
+				secondInput="${OPTARG}"
+				;;
+			t)
+				term="${OPTARG}"
+				;;
+			h)
+				
+				;;
+			\?)
+				echo "Invalid option: $OPTARG"
+				
+				;;
+			:)
+				echo "Option -$OPTARG requires an argument."
+				;;
+		esac
+	done
+	shift $((OPTIND -1))
+
 	# Do something with parsed arguments for testing
 	echo "Input path: $inputPath"
 	echo "Output path: $outputPath"
