84b386a18bf731c90c3944564f64c69deafc493c
Caleb Manor <calebmanorprogramming@gmail.com>
2024-06-11 18:48:01 +0000

    Added help message functionality and corrected minor syntax errors

--- convertPipelineToEcho.sh
+++ convertPipelineToEcho.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+usage() {
+	echo "Usage: $0 [-i <inputPath>] [-o <outputPath>] [-d] [-e <secondInput>] [-t <term>]"
+	echo "Options:"
+	echo " -i <inputPath>   Path to input csv file."
+	echo " -o <outputPath>  Path to output file. (Default: ./subjToDept.json)"
+	echo " -d               Only use this flag if you need to regenerate subjToDept.json"
+	echo " -e <secondInput> Takes a second path to try and determine name/email mapping. Pass Echo csv file to i argument."
+	echo " -t <term>        The term for the converted file. (e.g. Fall 2021)"
+	exit 1
+}
+
 main() { 
 	inputPath=""
 	outputPath="./subjToDept.json"
@@ -16,8 +27,8 @@ main() {
 			o)
 				outputPath="${OPTARG}"
 				;;
-			d)
-				getDept=true
+			d)	
+				getDepts=true
 				;;
 			e)
 				secondInput="${OPTARG}"
@@ -26,14 +37,15 @@ main() {
 				term="${OPTARG}"
 				;;
 			h)
-				
+				usage				
 				;;
 			\?)
 				echo "Invalid option: $OPTARG"
-				
+				usage	
 				;;
 			:)
 				echo "Option -$OPTARG requires an argument."
+				usage
 				;;
 		esac
 	done
