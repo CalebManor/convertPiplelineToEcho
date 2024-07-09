a9db40b12061557a8b5920fbca9726472f13e994
Caleb Manor <calebmanorprogramming@gmail.com>
2024-07-09 18:54:00 +0000

    Made revisions and added descriptive documentation

--- convertPipelineToEcho.sh
+++ convertPipelineToEcho.sh
@@ -7,32 +7,40 @@ convertCSV() {
 	
 	declare -A subjToDept
 	declare -A nameToEmail
+	
+	# Load JSON mappings into corresponding associative arrays
+	# Uses jq for easier JSON processing
+	# https://github.com/bobbyiliev/introduction-to-bash-scripting/blob/main/ebook/en/content/018-working-with-json-in-bash-using-jq.md
 
-	while IFS="=" read -r key value; do
-		subjToDept[$key]=$value
+	while IFS="=" read -r subject department; do
+		subjToDept[$subject]=$department
 	done < <(jq -r 'to_entries[] |"\(.key)=\(.value)"' subjToDept.json)
 
-	while IFS="=" read -r key value; do
-		nameToEmail[$key]=$value
+	while IFS="=" read -r instructorName email; do
+		nameToEmail[$instructorName]=$email
 	done < <(jq -r 'to_entries[] | "\(.key)=\(.value)"' nameToEmail.json)
 
 	local org="HU - Main Campus"
+	
+	# Write header row to output Echo file
+	echo "Organization, Department, Course Code, Course Name, Term,  Section Code, Primary Instructor Email, Secondary Instructor Emails" > "$outputEcho"
 
-	echo "Organization, Department, Course Code, Course Name, Section Code, Primary Instructor Email, Secondary Instructor Emails" > "$outputEcho"
-
+	# Reads and processes each row from inputCSV and writes the converted data to outputEcho
 	IFS=','
-	while read -r DEPT SUBJ TITLE INSTR; do
-		if [[ "$DEPT" == "Dept" ]]; then
+	while read -r CAMPUS SUBJ SECT TITLE INSTR; do
+		if [[ "$CAMPUS" == "CAMPUS" ]]; then
 			continue 
 		fi
 	
 		local organization="$org"
 		local dept="${subjToDept[$SUBJ]:-}"
+		local sectionCode="${SECT##*.}"
 		local courseCode="$SUBJ"
 		local className="$TITLE"
 		local email="${nameToEmail[$INST]:-TBA}"
 
-		echo "$orgName, $dept, $courseCode, $className, $term,, $email" >> "$outputEcho" 
+		echo "$organization, $dept, $courseCode, $className, $term, $sectionCode, $email"
+			 >> "$outputEcho" 
 	done < "$inputCSV"	
 }
 
@@ -43,9 +51,7 @@ usage() {
 	echo " -o <outputPath>  Path to output file. (Default: ./subjToDept.json)"
 	echo " -d               Only use this flag if you need to regenerate subjToDept.json"
 	echo " -e <secondInput> Takes a second path to try and determine name/email mapping. Pass Echo CSV file to i argument."
-	echo " -t <term>        The term for the converted file. (e.g. Fall 2021)"
-	exit 1
-}
+	echo " -t <term>        The term for the converted file. (e.g. Fall 2021)" exit 1 }
 
 main() { 
 	local inputEchoCSVPath=""
